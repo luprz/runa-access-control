e79bb0e71577beba373d8e2e76a7334452353237
luprz <luis.prz7@gmail.com>
2019-07-04 17:49:05 -0500

    Fix vulnerable gem

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope :api do
     scope :v1 do
       # Authentication
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'users'
     end
   end
 
