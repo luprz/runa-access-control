546e67dc77e02be7222e7468cdc5dcf72116cd13
luprz <luis.prz7@gmail.com>
2019-07-04 17:39:32 -0500

    Rspec for user authentication

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'database' do
     context 'columns' do
+      it { should have_db_column(:name).of_type(:string) }
       it { should have_db_column(:email).of_type(:string) }
-      it { should have_db_column(:access_token).of_type(:string) }
+      it { should have_db_column(:encrypted_password).of_type(:string) }
+      it { should have_db_column(:uid).of_type(:string) }
     end
   end
 end
