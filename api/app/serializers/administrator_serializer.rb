abc39729ed7d2d1242634b23d40d00b4f9c85fb7
luprz <luis.prz7@gmail.com>
2019-07-05 16:18:54 -0500

    Changed and add the relationships in administrator serializer

--- api/app/serializers/administrator_serializer.rb
+++ api/app/serializers/administrator_serializer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # User json api serializer
-class UserSerializer < ActiveModel::Serializer
+class AdministratorSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :position
+  has_many :employees
 end
